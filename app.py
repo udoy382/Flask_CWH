96b7faf74b057b83ff7153fee5bbcd9f3ff4c69e
Udoy <srudoy436@gmail.com>
2022-07-08 19:51:52 +0600

    first commit

---
+++ app.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    sno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    description = db.Column(db.String(500), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f"{self.sno} - {self.title}"
+
+
+@app.route("/", methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+
+        # todo = Todo(title='First Todo', desc='Start investing in stock market')
+        todo = Todo(title=title, description=description)
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodo = Todo.query.all()
+    # print(allTodo)
+
+    return render_template('index.html', allTodo=allTodo)
+
+
+@app.route("/update/<int:sno>", methods=['GET', 'POST'])
+def update(sno):
+    if request.method == 'POST':
+        title = request.form['title']
+        description = request.form['description']
+        todo = Todo.query.filter_by(sno=sno).first()
+        todo.title = title
+        todo.description = description
+        db.session.add(todo)
+        db.session.commit()
+        return redirect('/')
+
+
+    todo = Todo.query.filter_by(sno=sno).first()
+    # allTodo = Todo.query.all()
+    # print(allTodo)
+    # return "This is products page"
+
+    return render_template('update.html', todo=todo)
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9999)
+
+
+@app.route("/delete/<int:sno>")
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9999)
+
+
+
+@app.route("/show")
+def products():
+    allTodo = Todo.query.all()
+    # print(allTodo)
+    return "This is products page"
+
+if __name__ == '__main__':
+    app.run(debug=True, port=9999)
